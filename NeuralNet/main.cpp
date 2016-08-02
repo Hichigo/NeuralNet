c6d39c1e26c48e37598e5f6d516fbf4c0c0e243c
Ivan <hichigo91@gmail.com>
2016-08-02 12:53:49 +0300

    all work

--- NeuralNet/main.cpp
+++ NeuralNet/main.cpp
@@ -4,10 +4,12 @@ using namespace std;
 void main() 
 {
 	const int numLayers = 4;
-	int a[numLayers] = { 2, 4, 5, 3 }; // out 4 error
+	int a[numLayers] = { 2, 4, 5, 4 };
 	double input[2] = { 1.5, 0 };
+
 	vector<double> ans;
 	ans.push_back(1.0);
+	ans.push_back(0.0);
 	ans.push_back(0.0);
 	ans.push_back(0.0);
 	
@@ -18,5 +20,5 @@ void main()
 	cout << net.layers[3].neurons[0].value << endl;
 	cout << net.layers[3].neurons[1].value << endl;
 	cout << net.layers[3].neurons[2].value << endl;
-	//cout << net.layers[3].neurons[3].value << endl;
+	cout << net.layers[3].neurons[3].value << endl;
 }
